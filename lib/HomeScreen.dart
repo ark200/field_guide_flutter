ac73e40b2b3ef62e0069c09467720006a82ff1d5
Arun Roy Kanjamala <33016987+ark200@users.noreply.github.com>
2020-02-12 10:20:31 +0530

    field guide home page updated with images and no image

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -12,10 +12,11 @@ class HomeScreen extends StatefulWidget
 
 class _HomeScreenState extends State<HomeScreen>
 {
-  
+
   List data = [];
   var name = [];
   var sciname = [];
+  var photograph =[];
   var url = [];
   var result;
 
@@ -25,11 +26,11 @@ class _HomeScreenState extends State<HomeScreen>
     var response = await   ApiCall.makeGetRequest("snake/all/");
 
     if(json.decode(response.body)["status"]) {
-  setState(() {
-  data = json.decode(response.body)["data"] as List;
-  print(response.statusCode);
-  print(data.length);
-  });
+      setState(() {
+        data = json.decode(response.body)["data"] as List;
+        print(response.statusCode);
+        print(data.length);
+      });
     }
 
     else
@@ -50,7 +51,7 @@ class _HomeScreenState extends State<HomeScreen>
       debugShowCheckedModeBanner: false,
       title: 'FieldGuide',
       theme: ThemeData(
-        primaryColor: Colors.white
+          primaryColor: Colors.white
 //        primarySwatch: Colors.white,
       ),
       home: Scaffold(
@@ -67,23 +68,62 @@ class _HomeScreenState extends State<HomeScreen>
       itemCount: data.length,
       itemBuilder:(context, index)
       {
-        return Card(
-          child: InkWell(
-            onTap: (){
+        photograph = data[index]["photographs"];
+        if(photograph.length>0)
+        {
+          return Card(
+            child: InkWell(
+              onTap: ()
+              {
 
-            },
-            child: ListTile(
-//              leading: Image(image: NetworkImage("http://18.191.40.18/u/"+data[index]["url"]),),
-              leading: Image(image:AssetImage("Images/isi.png")),
-//              leading: Icon(Icons.image),
-              title: Text(data[index]["name"]),
-              subtitle: Text(data[index]["scientificName"]),
+              },
+              child: ListTile(
+                leading: Image(image: NetworkImage("http://18.191.40.18/u/"+data[index]["photographs"][0]["url"]),
+                  width: 100,
+                  height: 100,),
+                title: Text(data[index]["name"]),
+                subtitle: Text(data[index]["scientificName"]),
+                trailing: Icon(Icons.arrow_right),
+              ),
             ),
-          ),
-        );
+          );
+        }
+        else
+        {
+          return Card(
+            child: InkWell(
+              onTap: ()
+              {
+
+              },
+              child: ListTile(
+                leading: Image(image: AssetImage("Images/noiamge.png"),
+                  height: 100,
+                  width: 100,),
+//                  leading: Icon(Icons.image),
+                title: Text(data[index]["name"]),
+                subtitle: Text(data[index]["scientificName"]),
+                trailing: Icon(Icons.arrow_right),
+              ),
+            ),
+          );
+        }
+//        return Card(
+//          child: InkWell(
+//            onTap: (){
+//
+//            },
+//            child: ListTile(
+////              leading: Image(image: NetworkImage("http://18.191.40.18/u/"+data[index]["photographs"[0]]["url"]),),
+//              leading: Image(image:AssetImage("Images/isi.png")),
+////              leading: Icon(Icons.image),
+//              title: Text(data[index]["name"]),
+//              subtitle: Text(data[index]["scientificName"]),
+//
+//            ),
+//          ),
+//        );
       },
     );
   }
 }
-
-
